b7beaf2fb73a58db7a9a5b3a2328cd3179a5005e
Colby Jamieson <colbyjamieson@outlook.com>
2022-03-20 13:57:46 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    package_dir={"": "src",
+                "DHT20": "DHT20"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
