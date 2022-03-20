56c21fb3c711177979cd2d845fa6891a877bc3bb
Colby Jamieson <colbyjamieson@outlook.com>
2022-03-20 11:51:14 -0600

    Delete dht20 (2).py

--- dht20 (2).py
+++
@@ -1,9 +0,0 @@
-from machine import I2C
-from dht20 import DHT20
-i2c = I2C(1)
-dht20 = DHT20(i2c)
-while True:
-    temper = dht20.dht20_temperature()
-    humidity = dht20.dht20_humidity()
-    print("temper : " + str(temper))
-    print("humidity : " + str(humidity)) 
