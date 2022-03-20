9619d023c1cdfc2625fe599ac903c6c18df43e2a
Colby Jamieson <colbyjamieson@outlook.com>
2022-03-20 13:53:25 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="colbychristian",
+    name="DHT20",
     version="0.0.1",
     author="Colby Jamieson",
     author_email="colbyjamieson@outlook.com",
