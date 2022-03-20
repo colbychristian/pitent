aed9a0cdf572af49bbe8c8eadf3ef36220bdba16
Colby Jamieson <colbyjamieson@outlook.com>
2022-03-20 13:50:35 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(name='Distutils',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-     )
+setuptools.setup(
+    name="colbychristian",
+    version="0.0.1",
+    author="Colby Jamieson",
+    author_email="colbyjamieson@outlook.com",
+    description="Grove DHT20 temp hum sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/colbychristian/pitent",
+    project_urls={
+        "DHT20": "https://github.com/colbychristian/pitent/tree/main/dht20",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
